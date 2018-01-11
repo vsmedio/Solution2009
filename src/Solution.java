0d8c788aef7f1fe4eaabef523768073f37a16eb1
Arvis Abromovics <info@vsmedio.lv>
2018-01-11 23:00:46 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+
+/*
+Как сериализовать static?
+*/
+public class Solution {
+    public static class ClassWithStatic implements Serializable {
+        public static String staticString = "it's test static string";
+        public int i;
+        public int j;
+    }
+
+    // must read
+    // http://javateaching.blogspot.ru/2011/12/serialization-static-field.html
+
+    public static void main(String[] args) {
+    }
+}
